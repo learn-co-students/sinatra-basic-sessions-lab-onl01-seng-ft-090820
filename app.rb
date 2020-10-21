5c33de8a3b06d3ee29a28f659908c64aaf84d798
am20dipi <am20dipi@alum.siena.edu>
2020-10-21 14:29:25 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,24 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    configure do 
+        enable :sessions #ENABLES SESSIONS
+        set :session_secret, "secret" #SETS SESSION_SECRET
+    end
+
+    get '/' do 
+        erb :index #RENDERS AN ERB FILE, INDEX.ERB
+    end
+
+    post '/checkout' do #CONTROLLER ACTION
+        @sessions = session
+        item = params["item"]
+        @sessions[:item] = item
+        #TAKE THE PARAMS FROM THE FORM IN INDEX.ERB AND ADDS IT TO THE SESSION HASH. 
+        #THE KEY IS AN ITEM AND THE VALUE SHULD THE BE THE ITEM THE USER ENTERED
+        #STORES THE SESSION HASH IN AN INSTANCE VARIABLE
+    end
+
 end
+
